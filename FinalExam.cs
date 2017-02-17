25af6207c3d20ed0f54df3b98d04235afbfffcac
Nagy Zsuzsanna <nagyzsuska@live.com>
2017-02-17 10:20:04 +0100

    Catch null reference exception in the function; two unit tests added

--- FinalExam.cs
+++ FinalExam.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 
 namespace FinalExam {
  public class Program {
+    // Create a function that takes a list of numbers and returns the second biggest element from it
     public static int FindTheSecondBiggest(List<int> list)
     {
-      list.Sort();
+      try
+      {
+        list.Sort();
+      }
+      catch (NullReferenceException e)
+      {
+        Console.WriteLine("Your argument is null.\n" + e);
+        Console.ReadKey();
+      }
       return list[list.Count - 2];
     }
     
     static void Main(string[] args)
     {
-      // Create a function that takes a list of numbers and returns the second biggest element from it
-      List<int> list = new List<int>() { 9, 199, 2, 15, 3, 4, 34, 5 };
+      List<int> filledList = new List<int>() { 9, 199, 2, 15, 3, 4, 34, 5 };
+      List<int> nullList = null;
+      Console.WriteLine(FindTheSecondBiggest(filledList));
+      Console.WriteLine(FindTheSecondBiggest(nullList));
     }
   }
 }
--- ProgramTests.cs
+++ ProgramTests.cs
@@ -1,17 +1,35 @@
-using FinalExam;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 
 namespace FinalExam.Tests {
   [TestClass()]
   public class ProgramTests {
     [TestMethod()]
-    public void FindTheSecondBiggestTest()
+    public void FindTheSecondBiggestTest_AreEqual()
     {
       List<int> list = new List<int>() { 4, 3, 2, 1 };
       int expected = 3;
       int actual = Program.FindTheSecondBiggest(list);
-      Assert.AreEqual(expected, actual);
+      Assert.AreEqual(expected, actual, "The returned number is not the second biggest.");
+    }
+
+    [TestMethod()]
+    public void FindTheSecondBiggestTest_WithWrongExcpectedNumber_AreNotEqual()
+    {
+      List<int> list = new List<int>() { 4, 3, 2, 1, 9 };
+      int expected = 1;
+      int actual = Program.FindTheSecondBiggest(list);
+      Assert.AreNotEqual(expected, actual);
+    }
+
+
+    [TestMethod()]
+    [ExpectedException(typeof(NullReferenceException))]
+    public void FindTheSecondBiggestTest_CheckArgumentNullException()
+    {
+      List<int> list = null;
+      Program.FindTheSecondBiggest(list);
     }
   }
 }
